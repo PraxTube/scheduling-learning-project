0014547e1e9e4c108924b8aad363a6d12114fe2f
PraxTube <lukinator28@gmail.com>
2024-06-08 15:20:35 +0200

    feat: Implement queue.c

--- src/queue.c
+++ src/queue.c
@@ -1,5 +1,6 @@
 #include "../lib/queue.h"
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,54 +16,60 @@ int queue_add(void *new_object, queue_object *queue) {
         ptr->next = tmp_ptr;
     }
 
-    queue->object = ptr;
+    queue->next = ptr;
     ptr->object = new_object;
 
     return 0;
 }
 
 void *queue_poll(queue_object *queue) {
-    queue_object *second_last = queue;
-    queue_object *current_ptr = queue;
-    while (1) {
-        current_ptr = current_ptr->next;
-        if (current_ptr->next == NULL) {
-            break;
-        }
-
-        second_last = current_ptr;
-    }
-
-    if (second_last->next == NULL) {
+    if (queue->next == NULL) {
         return NULL;
     }
 
+    queue_object *second_last = queue;
+    queue_object *current_ptr = queue;
+    while (current_ptr->next != NULL) {
+        second_last = current_ptr;
+        current_ptr = current_ptr->next;
+    }
+
     void *last_element = second_last->next->object;
+    free(second_last->next);
     second_last->next = NULL;
 
     return last_element;
 }
 
 queue_object *new_queue() {
-    // TODO
-    return NULL;
+    queue_object *queue = (queue_object *)malloc(sizeof(queue_object));
+    if (queue == NULL) {
+        return NULL;
+    }
+    return queue;
 }
 
 void free_queue(queue_object *queue) {
     if (queue == NULL) {
-        // ?
+        return;
     }
 
-    while (queue->next != NULL) {
-        queue_object *previous_item = queue;
-        queue = queue->next;
-        free(previous_item);
+    queue_object *current_ptr = queue;
+
+    while (current_ptr != NULL) {
+        queue_object *previous_ptr = current_ptr;
+        current_ptr = current_ptr->next;
+        free(previous_ptr);
     }
 }
 
 void *queue_peek(queue_object *queue) {
-    if (queue == NULL) {
-        return NULL;
+    queue_object *current_ptr = queue;
+
+    while (1) {
+        if (current_ptr->next == NULL) {
+            return current_ptr->object;
+        }
+        current_ptr = current_ptr->next;
     }
-    return queue->object;
 }

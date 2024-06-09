7d974ea8c4a09bcf6bc577d78c48e1dc682561e9
PraxTube <lukinator28@gmail.com>
2024-06-09 16:35:04 +0200

    feat: Finish LCFS implementation

--- src/LCFS.c
+++ src/LCFS.c
@@ -4,20 +4,38 @@ static queue_object *LCFS_queue;
 // You can add more global variables here
 
 int LCFS_startup() {
-    // TODO
+    LCFS_queue = new_queue();
+    if (LCFS_queue == NULL) {
+        return 1;
+    }
     return 0;
 }
 
 process *LCFS_tick(process *running_process) {
-    // TODO
-    return NULL;
+    if (running_process == NULL || running_process->time_left == 0) {
+        if (LCFS_queue->next == NULL) {
+            return NULL;
+        }
+
+        running_process = LCFS_queue->next->object;
+        if (LCFS_queue->next->next == NULL) {
+            LCFS_queue->next = NULL;
+        } else {
+            LCFS_queue->next = LCFS_queue->next->next;
+        }
+    }
+    if (running_process != NULL) {
+        running_process->time_left--;
+    }
+
+    return running_process;
 }
 
 process *LCFS_new_arrival(process *arriving_process, process *running_process) {
-    // TODO
-    return NULL;
+    if (arriving_process != NULL) {
+        queue_add(arriving_process, LCFS_queue);
+    }
+    return running_process;
 }
 
-void LCFS_finish() {
-    // TODO
-}
+void LCFS_finish() { free_queue(LCFS_queue); }
